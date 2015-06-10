23f19e5b42a2ce9d0d6900139ba7512c4cebb4b0
Takayasu Oyama <takayasu.oyama@playnext.co.jp>
2015-06-10 11:57:18 +0900

    update rooms in realtime

--- app.js
+++ app.js
@@ -1,46 +1,58 @@
 var express = require('express');
 var app = express();
-app.set('view engine', 'ejs');
-app.set('views', './views');
-app.use(express.static('public'));
-
 var server = require('http').Server(app);
 var io = require('socket.io')(server, { pingInterval: 100000000 });
 
 var sndPrefix = "room:s:";
-var recPrefix = "room:r:"
+var recPrefix = "room:r:";
 
+// HTTP Stuff
 server.listen(3030);
-
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
 app.get('/', function (req, res) {
   res.render('index', { rooms: io.sockets.adapter.rooms });
 });
 
-io.on('connection', function (socket) {
+// Socket.IO Stuff
+io.on("connection", function (socket) {
   socket.on("create", function(data, ackCallback) {
-    console.log(data);
+    // create/join a room for sending logs
     var sRoom = sndPrefix + data.room;
-    var rRoom = recPrefix + data.room;
     socket.join(sRoom);
+    console.log(socket.conn.id + " joined room: " + sRoom);
+
+    // hook event for sending logs to viewers
+    var rRoom = recPrefix + data.room;
     socket.on("log", function(data) {
       console.log(data);
       io.to(rRoom).emit("logged", data);
     });
+
+    // Let viewers know room was created
+    if(!io.sockets.adapter.rooms[rRoom]) {
+      io.emit("created", { room: rRoom.replace("room:r:", "") });
+    }
+
+    // send ACK to client so it can start sending logs
     ackCallback();
   });
 
   socket.on("join", function(data) {
     var rRoom = recPrefix + data.room;
-    console.log('joined: ' + socket.conn.id + " => " + rRoom);
-    socket.join(recPrefix + data.room);
+    console.log("joined: " + socket.conn.id + " => " + rRoom);
+    socket.join(rRoom);
   });
 
   socket.on("leave", function(data) {
-    socket.leave(sndPrefix + data.room);
-    socket.leave(recPrefix + data.room);
+    var rRoom = recPrefix + data.room;
+    console.log("left: " + socket.conn.id + " => " + rRoom);
+    socket.leave(rRoom);
   });
 
-  socket.on('disconnect', function () {
-    console.log('disconnected: ' + socket.conn.id);
+  socket.on("disconnect", function () {
+    console.log(socket.rooms);
+    console.log("disconnected: " + socket.conn.id);
   });
 });
