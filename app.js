be7e42e2b8ba2d38566261a650b02c85cd113929
Takayasu Oyama <takayasu.oyama@playnext.co.jp>
2015-06-17 21:59:04 +0900

    no server logic in client code

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var port = 3030;
+var _ = require('underscore');
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server, { pingInterval: 100000000 });
+var port = 3030;
+
+var LogReceiver = require('./log_receiver');
+var LogSender = require('./log_sender');
+var senders = LogSender.rooms;
+var recievers = LogReceiver.rooms;
 
 // HTTP Stuff
 server.listen(port);
@@ -11,13 +17,11 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 app.get('/', function (req, res) {
-  res.render('index', { roomIds: Object.keys(io.sockets.adapter.rooms) || [] });
+  var roomIds = Object.keys(io.sockets.adapter.rooms);
+  var rRoomIds = _.filter(roomIds, function(id) { return id.indexOf(LogReceiver.prefix) >= 0; });
+  var rooms = _.map(rRoomIds, function(id) { return id.replace("room:r:", ""); }).concat("all");
+  res.render('index', { rooms: _.uniq(rooms) || [] });
 });
-
-var LogReceiver = require('./log_receiver');
-var LogSender = require('./log_sender');
-var senders = LogSender.rooms;
-var recievers = LogReceiver.rooms;
 
 // Socket.IO Stuff
 io.on("connection", function (socket, connCallback) {
